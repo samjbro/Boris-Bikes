504a948fa1a570ee5993457237b7f93d582fd404
Giancarlo Fabrizi <gfabrizi@Giancarlos-Air.lan>
2016-07-06 01:06:21 +0200

    building bike, test passed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,6 @@
 class DockingStation
-<<<<<<< HEAD
-def release_bike
-end
 
-=======
   def release_bike
-    
   end
->>>>>>> 4ef7e57ed2a8205d3f5dc30bc84e81cdd3c88ad9
+
 end
---
+++ spec/bike_spec.rb
@@ -0,0 +1,5 @@
+require 'bike'
+describe Bike do
+  bike = Bike.new
+  it "is working" do expect(bike.working?).to eq true end
+  end
