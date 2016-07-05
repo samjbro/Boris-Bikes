809f7e421bf9b8f07bf8f186e6fc2ce21e142b09
6eff <6eff@mail.ru>
2016-07-05 20:03:13 +0300

    def and test bike? and working?

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,6 @@ require 'docking_station'
 
 describe DockingStation do
 		it {expect(DockingStation.new).to respond_to(:release_bike)}
+    it {expect(subject.release_bike).to be_a(Bike)}
+    it {expect(subject.release_bike).to be_working}
 end
