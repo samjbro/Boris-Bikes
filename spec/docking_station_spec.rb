44d4245eabb9017fe38102278dce0979ce717a0f
cyberplanner <saleh.gfx@gmail.com>
2016-07-06 12:49:15 +0100

    2 new tests: gets a bike, expects the bike to be working..Passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,10 @@
 require 'docking_station'
 describe DockingStation do
   it "Releases a bike" do should respond_to(:release_bike) end
+  it "gets a bike" do
+    expect(subject.release_bike).to be_a(Bike)
+  end
+  it "expects bike to be working" do
+    expect(subject.release_bike).to be_working
+  end
 end
