c6150b45e11b69b2244a9b14602d4808e984c6ee
samjbro <sjbroughton1@gmail.com>
2016-07-06 12:49:35 +0100

    fixed spec file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,8 +5,6 @@ describe DockingStation do
     it {expect(subject.release_bike).to be_a(Bike)}
     it {expect(subject.release_bike).to be_working}
     it {expect(subject).to respond_to(:dock)}
-    describe subject.docked_bikes do
-    	it {expect(subject).to be_a(Array)}
-  	end
+   
 
 end
