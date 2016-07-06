3f0e11626900bd422b5af87783b3fc65d9837a06
samjbro <sjbroughton1@gmail.com>
2016-07-06 16:23:51 +0100

    dock_bike fails if docking station is full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,6 @@
 require 'docking_station'
 describe DockingStation do
+  
   describe '#release_bike' do
 	  it "Release bike method exists" do 
 	  	should respond_to(:release_bike) 
@@ -19,6 +20,16 @@ describe DockingStation do
 	    expect(subject.release_bike).to be_working
 	  end
 	end
+
+	describe '#dock_bike' do
+		it "raises an error" do
+			bike1 = Bike.new
+			bike2 = Bike.new
+			subject.dock_bike(bike1)
+			expect{subject.dock_bike(bike2)}.to raise_error("Docking station full")
+		end
+	end
+
   it {should respond_to(:dock_bike).with(1).argument}
 
   it "should see a docked bike" do
