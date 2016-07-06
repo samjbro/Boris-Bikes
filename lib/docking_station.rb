9928032fca979d8471961d6620bd68c506de65fd
samjbro <sjbroughton1@gmail.com>
2016-07-06 12:09:22 +0100

    final changes for tuesday

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,13 @@
 require_relative './bike'
 class DockingStation
+	attr_accessor :docked_bikes
+	def initialize
+		@docked_bikes = Array.new
+	end
 	def release_bike
 		Bike.new
+	end
+	def dock bike
+
 	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,9 @@ describe DockingStation do
 		it {expect(DockingStation.new).to respond_to(:release_bike)}
     it {expect(subject.release_bike).to be_a(Bike)}
     it {expect(subject.release_bike).to be_working}
+    it {expect(subject).to respond_to(:dock)}
+    describe subject.docked_bikes do
+    	it {expect(subject).to be_a(Array)}
+  	end
+
 end
