01f862b3d54db7aa2f1e3aeae4beb6e60a6c9ce3
Titus Iudean <iudeant@gmail.com>
2016-07-07 18:38:24 +0100

    Code handles multiple bikes with a maximum number of 20

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,20 @@
 require_relative 'bike.rb'
 
 class DockingStation
-  attr_accessor :bike
+
+  attr_accessor :bikes
+
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    raise "No bikes docked" unless @bike
-    @bike
+    raise "No bikes docked" if @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    raise 'Docking station full' if @bike
-    @bike = bike
+    raise 'Docking station full' if @bikes.length >= 20
+    @bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
+  it 'initializes bikes to an empty array' do
+    expect(subject.bikes).to eq []
+  end
   it 'release_bike returns an error if there are no bikes available' do
     expect{subject.release_bike}.to raise_error "No bikes docked"
   end
@@ -10,13 +13,13 @@ describe DockingStation do
   it 'docks a bike into the docking station' do
     expect(subject).to respond_to(:dock).with(1).argument
   end
-  it 'dock method returns an error if there is one bike already in the docking station' do
-    subject.dock(Bike.new)
+  it 'dock method returns an error if there are 20 bikes already in the docking station' do
+    20.times {subject.dock(Bike.new)} 
     expect{subject.dock(Bike.new)}.to raise_error 'Docking station full'
   end
   it 'returns a docked bike' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes.last).to eq bike
   end
 end
