7153322bc971527e7c7f63b6ec27e368c3c3581f
Titus Iudean <iudeant@gmail.com>
2016-07-07 15:35:25 +0100

    Adds functionality to dock a bike into the station and view the docked bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,13 @@
 require_relative 'bike.rb'
 
 class DockingStation
+  attr_accessor :bike
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
   end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,12 @@ describe DockingStation do
   it 'releases bike from docking station' do
     expect(DockingStation.new).to respond_to :release_bike
   end
+  it 'docks a bike into the docking station' do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+  it 'returns a docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
