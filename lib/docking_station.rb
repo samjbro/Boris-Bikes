244c783c3fb34014964d4e64cb415251edbeacd7
Titus Iudean <iudeant@gmail.com>
2016-07-07 17:52:17 +0100

    Tests for docking station full and raises exception if full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,15 +3,12 @@ require_relative 'bike.rb'
 class DockingStation
   attr_accessor :bike
   def release_bike
-    # Bike.new
-    if @bike == nil
-      raise "Errorrrr"
-    else
-      @bike
-    end
+    raise "No bikes docked" unless @bike
+    @bike
   end
 
   def dock(bike)
+    raise 'Docking station full' if @bike
     @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,13 +2,17 @@ require 'docking_station'
 
 describe DockingStation do
   it 'release_bike returns an error if there are no bikes available' do
-    expect{subject.release_bike}.to raise_error "Errorrrr"
+    expect{subject.release_bike}.to raise_error "No bikes docked"
   end
   it 'releases bike from docking station' do
     expect(DockingStation.new).to respond_to :release_bike
   end
   it 'docks a bike into the docking station' do
     expect(subject).to respond_to(:dock).with(1).argument
+  end
+  it 'dock method returns an error if there is one bike already in the docking station' do
+    subject.dock(Bike.new)
+    expect{subject.dock(Bike.new)}.to raise_error 'Docking station full'
   end
   it 'returns a docked bike' do
     bike = Bike.new
